f41706b012ae2b1ba133f62bc4ffe6b50987a4dd
reza <rezakarimi74pnu@gmail.coma>
2024-04-24 23:28:55 +0330

    box2finish

--- src/component/landing/box2/Box2.jsx
+++ src/component/landing/box2/Box2.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 import '../box1/Box1.modular.css';
-
+import icon1 from '../../../assets/img/icon1.png';
+import icon2 from '../../../assets/img/icon2.png';
+import icon3 from '../../../assets/img/icon3.png';
 
 const Box2 = () => {
     return (
         <div className=" h-[45.7rem] back2 ">
             <div className="h-[45.7rem] bg-[#0000006b] relative">
-                <div className=" h-80 absolute top-[25.6rem] w-full bg-[#0000006b]">
-                    <div className="h-80 w-[31rem] inline-block">
-                        <div className="border border-solid border-red-500 h-14 w-14 m-auto mt-11">  </div>
-                        <div className="border border-solid border-red-500 h-16 w-52 m-auto mt-6"> </div>
-                        <div className="border border-solid border-red-500 h-20 w-[27rem] m-auto mt-6"> </div>
+                <div className=" h-80 absolute top-[25.6rem] w-full bg-[#0000006b] flex">
+
+
+
+                    <div className="h-80 w-[32rem] inline-block">
+                        <div className=" h-12 w-12 m-auto mt-16">
+                            <img src={icon1} className="h-12 w-12" />
+                        </div>
+                        <div className=" h-16 w-52 m-auto mt-6 flex"> 
+                           <div className=" h-14 w-32 text-white text-4xl text-end"> دوره ها </div>
+                           <div className=" h-14 w-24 text-white text-4xl text-center"> 219 </div>
+                        </div>
+                        <div className=" h-20 w-[18rem] m-auto mt-3 text-white text-center text-sm "> تا کنون بیش از ۲۰۰ دوره از دسته بندی های متفاوت در سایت ثبت شده و قابل دسترسی است </div>
                     </div>
+
+
                     <div className="border border-solid border-[#dbdbdb91] h-80 inline-block"> </div>
-                    <div className="h-80 w-[31rem] inline-block">
-                        <div className="border border-solid border-red-500 h-14 w-14 m-auto mt-11"> </div>
-                        <div className="border border-solid border-red-500 h-16 w-52 m-auto mt-6"> </div>
-                        <div className="border border-solid border-red-500 h-20 w-[27rem] m-auto mt-6"> </div>
+
+
+
+                    <div className="h-80 w-[32rem] inline-block">
+                    <div className=" h-12 w-12 m-auto mt-16">
+                            <img src={icon2} className="h-12 w-12" />
+                        </div>
+                        <div className=" h-16 w-52 m-auto mt-6 flex"> 
+                           <div className=" h-14 w-32 text-white text-4xl text-end"> استاد </div>
+                           <div className=" h-14 w-24 text-white text-4xl text-center"> 34 </div>
+                        </div>
+                        <div className=" h-20 w-[19rem] m-auto mt-3 text-white text-center text-sm "> بیش از ۳۰ استاد از بهترین اساتید ایران همراه ما هستن و دوره های خودشون و ثبت کردن </div>
                     </div>
-                    <div className="border border-solid border-[#dbdbdb91] h-80 inline-block"> </div>
-                    <div className=" border border-solid border-red-500 h-80 w-[31rem] inline-block">
-                        <div className=" text-white inline"> </div>
+
+
+                    <div className="border border-solid border-[#dbdbdb91] h-80 inline-block">  </div>
+
+
+
+                    <div className=" h-80 w-[32rem] inline-block">
+                    <div className=" h-12 w-12 m-auto mt-16">
+                            <img src={icon3} className="h-12 w-12" />
+                        </div>
+                        <div className=" h-16 w-52 m-auto mt-6 flex"> 
+                           <div className=" h-14 w-32 text-white text-4xl text-end"> دانشجو </div>
+                           <div className=" h-14 w-24 text-white text-4xl text-center"> 576 </div>
+                        </div>
+                        <div className=" h-20 w-[19rem] m-auto mt-3 text-white text-center text-sm "> تا کنون بیش از ۵۰۰ نفر از اموزش های بامبو استفاده کرده اند و نظرات خودشونو ثبت کرده اند </div>
                     </div>
                 </div>
             </div>
