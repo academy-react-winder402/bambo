c654746a160d0354f7ccb32c7dc9e7389ff8aa8a
reza <rezakarimi74pnu@gmail.coma>
2024-04-26 03:10:22 +0330

    box4start

---
+++ src/component/landing/box4/Box4.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Box4 = () => {
+    return(
+        <div className="border border-solid border-red-500 h-[45rem]">
+            <div className="border border-solid border-red-500 h-[15rem] bg-[#004458]">  </div>    
+            <div className="border border-solid border-red-500 h-[15rem] bg-">  </div>   
+            <div className="border border-solid border-red-500 h-[15rem] bg-[#004458]">  </div>   
+        </div>
+    );
+};
+
+export {Box4};
