f4fb6efdb425a2ff4119dee8fc9b99f0ee9df7e6
reza <rezakarimi74pnu@gmail.coma>
2024-04-25 22:53:28 +0330

    Box3

--- src/component/landing/Landingc.jsx
+++ src/component/landing/Landingc.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Box1 } from "./box1/Box1";
 import {Box2} from './box2/Box2';
+import {Box3} from './box3/Box3';
 
 const Landingc = () => {
     return (
         <div>
             <Box1 />
             <Box2 />
+            <Box3 />
         </div>
 
     );
