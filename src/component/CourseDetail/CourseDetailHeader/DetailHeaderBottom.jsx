67bb318172b4b1bbca6273f52d51ffca676f1b29
reza <rezakarimi74pnu@gmail.coma>
2024-05-02 01:46:43 +0330

    hedercour

--- src/component/CourseDetail/CourseDetailHeader/DetailHeaderBottom.jsx
+++ src/component/CourseDetail/CourseDetailHeader/DetailHeaderBottom.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const DetailHeaderBottom = () => {
     return(
- <div className="w-[100%] h-[300px] border-solid border-[#DBDBDB] border-[1px] flex flex-row">
-<div className="w-[33%] h-[300px] border-solid border-[#DBDBDB] border-[1px]"></div>
-<div className="w-[33%] h-[300px]  border-solid border-[#DBDBDB] border-[1px]"></div>
-<div className="w-[33%] h-[300px]  border-solid border-[#DBDBDB] border-[1px]"></div>
+ <div className="w-[100%] h-[300px] border-solid border-[#DBDBDB] border-[1px] flex flex-row-reverse text-center text-[40px] leading-[30s0px]">
+<div className="w-[33.5%] h-[300px] border-solid border-[#DBDBDB] border-[1px]">مدرس : دکتر بحرالعلومی</div>
+<div className="w-[33.5%] h-[300px]  border-solid border-[#DBDBDB] border-[1px]"></div>
+<div className="w-[33.5%] h-[300px]  border-solid border-[#DBDBDB] border-[1px]">رتبه:</div>
  </div>
     );
 };
