dc3452fbbf7e8b8dd5b8cc659e8df6e0fa5cb4ce
reza <rezakarimi74pnu@gmail.coma>
2024-05-01 21:39:26 +0330

    articles1

--- src/component/articles/Articlesc.jsx
+++ src/component/articles/Articlesc.jsx
@@ -4,11 +4,11 @@ import {Footer} from "../common/Footer"
 import { ArticleContent } from "./ArticleContent";
 const Articlesc = () => {
     return(
-        <div className="w-[1536px] h-[2648px] border-solid border-[1px] border-black">
+        <>
             <Header/>
             <ArticleContent/>
        
-        </div>
+        </>
     );
 };
 
