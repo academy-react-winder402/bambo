ed5bf4f95a8f58c2922dd78f80053b68542cae95
reza <rezakarimi74pnu@gmail.coma>
2024-05-02 16:45:26 +0330

    hyu

--- src/component/CourseDetail/CourseDetailContant/DetailContantBottom/DetailContantBottom.jsx
+++ src/component/CourseDetail/CourseDetailContant/DetailContantBottom/DetailContantBottom.jsx
@@ -3,8 +3,13 @@ import React from "react";
 
 const DetailContantBottom = () => {
     return(
-<div className="h-[670px] border-solid border-[red] border-[1px]">
-    
+<div className="h-[600px] border-solid border-[red] border-[1px] flex flex-row-reverse">
+    <div className="w-[600px]
+     border-solid border-[green] border-[1px]
+      bg-[url('assets/img/coursedetail/bottomdetail.jpg')] bg-cover"></div>
+    <div className="w-[950px] bg-[#F9F9F9] direction">
+        <div className="w-[400px] h-[80px] text-[50px] text-[#004458] text-right">مزایای این دوره</div>
+    </div>
 </div>
     );
 };
