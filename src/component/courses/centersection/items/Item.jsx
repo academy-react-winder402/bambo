01ea13229b598b90b61b1a85f0ebaa1d38d924ee
reza <rezakarimi74pnu@gmail.coma>
2024-05-06 19:37:45 +0330

    coursesfinishh

--- src/component/courses/centersection/items/Item.jsx
+++ src/component/courses/centersection/items/Item.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Top } from "./top/Top";
 import {Bottom} from "./bottom/Bottom";
 
-const Item = ({im,title,modares,zarfiat,price,bg}) => {
+const Item = ({im,title,modares,zarfiat,price}) => {
     return (
         <div className=" w-[16.5rem]  mb-[3rem] mr-7 hover:shadow-2xl hover:scale-110 transition duration-300 ">
-            <Top img={im} bg={bg}/>
+            <Top img={im}/>
             <Bottom title={title} modares={modares} zarfiat={zarfiat} price={price}  />
         </div>
     );
--- src/component/courses/centersection/items/top/Top.jsx
+++ src/component/courses/centersection/items/top/Top.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-const Top = ({img,bg}) => {
+const Top = ({img}) => {
     return (
-        <div className={`h-[15rem] rounded-t-lg bg-[${bg}]`} >  
+        <div className="h-[15rem] rounded-t-lg" >  
         
             <img src={img} className="h-[15rem] w-[17rem] rounded-t-lg "/>
         </div>
