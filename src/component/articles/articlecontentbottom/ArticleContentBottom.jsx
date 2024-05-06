8585159493938e7d7651aa513b768997c9c007ae
reza <rezakarimi74pnu@gmail.coma>
2024-05-06 19:54:21 +0330

    articlestart

--- src/component/articles/articlecontentbottom/ArticleContentBottom.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const ArticleContentBottom =()=>{
-    return (
-        <div className="w-[380px] h-[74px] border-[red] border-[1px] border-solid m-auto mt-[50px]"></div>
-    )
-}
-export {ArticleContentBottom}
