99f6f1bc88023dad08c7713b58301480e4da4006
reza <rezakarimi74pnu@gmail.coma>
2024-05-06 19:30:56 +0330

    coursedetailcomend

--- src/component/CourseDetail/comment/CommentCourses.jsx
+++ src/component/CourseDetail/comment/CommentCourses.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { CommentBottom } from "./CommentBottom";
 const CommentCourses =()=>{
+    const comments ={
+        id:1,
+        item:[
+            {id:1677252427307,
+                name:"hello",
+                item:[
+                    {
+                        id:177252434572,
+                        name:"hello world",
+                        item:[]
+                    }
+                ]
+            }
+        ]
+    };
+    const [commentsData,setCommentsData]=useState(comments);
     return(
         <div className=" mt-[50px] h-[700px] bg-[#F9F9F9]" >
             <div className="h-[100px]  flex  gap-[800px]  ">
@@ -10,7 +26,7 @@ const CommentCourses =()=>{
   
             </div>
             <div>
-            <CommentBottom/>
+            <CommentBottom comment={commentsData}/>
             </div>
 
         </div>
