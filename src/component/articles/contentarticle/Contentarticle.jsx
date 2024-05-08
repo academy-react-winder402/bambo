6df685862ff32567e79b0067aa0ad42392fd6109
reza <rezakarimi74pnu@gmail.coma>
2024-05-08 23:02:48 +0330

    articlefinishlast

--- src/component/articles/contentarticle/Contentarticle.jsx
+++ src/component/articles/contentarticle/Contentarticle.jsx
@@ -61,6 +61,12 @@ const Contentarticle = () => {
         setpage(id);
     };
 
+    const [itemm, setitem] = useState(2);
+
+    const updateitem = (id) => {
+        setitem(id);
+    };
+
 
 
 
@@ -69,16 +75,61 @@ const Contentarticle = () => {
             <div className="h-[6rem] w-full flex justify-between">
 
                 <div className="  h-[3rem] w-[10rem] flex gap-[0.5rem] mt-[2rem] ml-[4rem]">
-                    <div className=" h-[3rem] w-[3rem] bg-[url(./././assets/img/courses/listamodi.png)]
-                                    bg-no-repeat bg-cover hover:cursor-pointer" onClick={() => updatetab(2)}> </div>
-                    <div className=" h-[3rem] w-[3rem] bg-[url(./././assets/img/courses/listofoghi.png)] 
-                                    bg-no-repeat bg-cover hover:cursor-pointer" onClick={() => updatetab(1)}> </div>
+                    <div className={page === 2 ? "h-[3rem] w-[3rem] bg-[url(./././assets/img/courses/listamodi.png)] bg-no-repeat bg-cover hover:cursor-pointer bg-[#fff]"
+                        : "h-[3rem] w-[3rem] bg-[url(./././assets/img/courses/listamodi.png)] bg-no-repeat bg-cover hover:cursor-pointer "} onClick={() => updatetab(2)}> </div>
+                    <div className={page === 1 ? " h-[3rem] w-[3rem] bg-[url(./././assets/img/courses/listofoghi.png)] bg-no-repeat bg-cover hover:cursor-pointer bg-[#fff]"
+                        : " h-[3rem] w-[3rem] bg-[url(./././assets/img/courses/listofoghi.png)] bg-no-repeat bg-cover hover:cursor-pointer"} onClick={() => updatetab(1)}> </div>
                 </div>
                 <h1 className="text-4xl inline-block mr-[4.5rem] mt-[1.5rem]"> اخبار و مقالات </h1>
 
             </div>
 
-            <div className="  mt-[3rem] pb-[1rem]">
+
+            <div className=" h-[3rem] w-[60rem] m-auto flex">
+
+                <div className={itemm === 2 ? " border-b-4 border-solid border-[#004458] h-[3rem] w-[30rem] text-center text-[#004458] text-3xl leading-10 hover:cursor-pointer" : " border-b-2 border-solid border-[#004458] h-[3rem] w-[30rem] text-center text-[#004458] text-2xl leading-10 hover:cursor-pointer"}
+                    onClick={() => { updateitem(2) }}> اخبار </div>
+                <div className={itemm === 1 ? " border-b-4 border-solid border-[#004458] h-[3rem] w-[30rem] text-center text-[#004458] text-3xl leading-10 hover:cursor-pointer" : " border-b-2 border-solid border-[#004458] h-[3rem] w-[30rem] text-center text-[#004458] text-2xl leading-10 hover:cursor-pointer"}
+                    onClick={() => { updateitem(1) }}> مقالات </div>
+
+            </div>
+
+
+
+            <div className={itemm === 2 ? "mt-[3rem] pb-[1rem]" : "hidden"}>
+                <div className=" w-[74rem] m-auto">
+                    <div className={page === 1 ? "flex flex-wrap" : "hidden"}>
+                        {
+                            visibleitem.map((item, index) => {
+                                return (<Grid2 key={index} im={item.im} title={item.title} />);
+                            })
+                        }
+                    </div>
+
+                    <div className={page === 2 ? "flex flex-wrap" : "hidden"}>
+                        {visibleitem.map((item, index) => {
+                            return (<Grid key={index} im={item.im} title={item.title} />);
+                        })}
+                    </div>
+
+
+
+                    <div className="  h-[2rem] flex gap-2 m-auto mb-[2rem] w-full justify-center">
+
+                        <div onClick={prevpagehandles} className="border border-solid border-[#D1D1D1] text-lg p-[0.6rem] leading-[0.5rem] hover:bg-[#004458] 
+                                                                    hover:cursor-pointer hover:text-white bg-[#fff] rounded-md"> prev </div>
+                        <p> {pages.map(page => <span key={page} onClick={() => setcurrentpage(page)}
+                            className="border border-solid border-[#D1D1D1] w-[2rem] h-[2rem] text-center leading-7 inline-block hover:bg-[#004458]
+                                        hover:cursor-pointer hover:text-white bg-[#fff]"> {`${page}`} </span>)} </p>
+                        <div onClick={nextpagehandles} className="border border-solid border-[#D1D1D1] text-lg p-[0.6rem] leading-[0.5rem] hover:bg-[#004458]
+                                                                hover:cursor-pointer hover:text-white bg-[#fff] rounded-md"> next </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div className={itemm === 1 ? "mt-[3rem] pb-[1rem] bg-red-500" : "hidden"}>
                 <div className=" w-[74rem] m-auto">
                     <div className={page === 1 ? "flex flex-wrap" : "hidden"}>
                         {
