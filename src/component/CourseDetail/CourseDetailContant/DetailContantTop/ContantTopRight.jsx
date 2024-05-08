7a471f96790a217843756c7175cd7ef181a30de7
reza <rezakarimi74pnu@gmail.coma>
2024-05-09 00:25:06 +0330

    comentdezin

--- src/component/CourseDetail/CourseDetailContant/DetailContantTop/ContantTopRight.jsx
+++ src/component/CourseDetail/CourseDetailContant/DetailContantTop/ContantTopRight.jsx
@@ -1,25 +1,37 @@
-import React from "react";
-import {CommentCourses  } from "../../comment/CommentCourses"
+import React, { useState } from "react";
 import { Tozihat } from "./Tozihat"
 import { CommentCourses } from "../../comment/CommentCourses";
-import { NavLink } from "react-router-dom";
+
+
+
+
 const ContantTopRight = () => {
-    const [page, setpage] = useState(1);
 
+const [bottom, setbottom] = useState(1);
 
+const updatebottom = (id) => {
+    setbottom(id);
+};
 
-  return(
+return (
 <div className=" w-[900px] border-solid  mt-[50px] flex flex-col">
     <div className="flex flex-row justify-end w-[900px]">
-    <div className="w-[140px] h-[70px] text-[30px] text-[#004458] text-center rounded-t-[0.8em] border-[1px] border-solid border-[#707070] border-b-[transparent] p-[10px] mr-[20px]">نظرات</div>
-    <div className="w-[140px] h-[70px] text-[30px] text-[#004458] text-center  rounded-t-[0.8em] border-[1px] border-solid border-[#707070] border-b-[transparent] p-[10px]">توضیحات</div>
+    <div className={bottom == 2 ? "bg-[#f9f9f9] w-[140px] h-[70px] text-[30px] text-[#004458] text-center rounded-t-[0.8em] border-[1px] border-solid  border-[#707070] border-b-[transparent] p-[10px] mr-[20px] hover:cursor-pointer" 
+    : "w-[140px] h-[70px] text-[30px] text-[#004458] text-center rounded-t-[0.8em] border-[1px] border-solid  border-[#004458] border-b-[transparent] p-[10px] mr-[20px] hover:cursor-pointer" } onClick={()=> {updatebottom(2)}}>نظرات</div>
+  <div className={bottom == 1 ? "bg-[#f9f9f9] w-[140px] h-[70px] text-[30px] text-[#004458] text-center rounded-t-[0.8em] border-[1px] border-solid  border-[#707070] border-b-[transparent] p-[10px] mr-[20px] hover:cursor-pointer" 
+    : "w-[140px] h-[70px] text-[30px] text-[#004458] text-center rounded-t-[0.8em] border-[1px] border-solid  border-[#004458] border-b-[transparent] p-[10px] mr-[20px] hover:cursor-pointer" } onClick={()=> {updatebottom(1)}}>توضیحات</div>
     </div>
-  <div>
 
+  <div className={bottom === 1 ? "bg-[#f9f9f9]" : "hidden"}>
+    <Tozihat />
+  </div>
+
+  <div className={bottom === 2 ? "bg-[#f9f9f9]" : "hidden"}>
+    <CommentCourses />
   </div>
 
 </div>
-    );
+);
 };
 
 export {ContantTopRight };
--- src/component/CourseDetail/comment/CommentCourses.jsx
+++ src/component/CourseDetail/comment/CommentCourses.jsx
@@ -4,6 +4,7 @@ const CommentCourses = () => {
     const [formState, setFormState] = useState({
         username: '',
         comment: '',
+     
     });
     const [comments, setComments] = useState([]);
     const users = [
@@ -21,7 +22,7 @@ const CommentCourses = () => {
         },
     ];
     const submit = () => {
-        if (formState.username === '' || formState.comment === '') {
+        if (formState.username === '' || formState.comment === '' ) {
             alert('Please fill in all fields');
             return;
         }
@@ -30,20 +31,22 @@ const CommentCourses = () => {
             {
                 username: formState.username,
                 comment: formState.comment,
+           
             },
         ]);
         setFormState({
             username: '',
             comment: '',
+          
         });
     };
     const current = new Date();
     const date = `${current.getDate()}/${current.getMonth() + 1
         }/${current.getFullYear()}`;
+ 
     return (
         <div className="flex flex-col justify-center align-middle w-[500px]  bg-[#f9f9f9]">
-            <section className="flex flex-col p-[1rem] w-[27rem]">
-                <h2 className="text-[#004458] text-[2rem] mb-[1rem]  text-center">پیشنهادات و انتقادات</h2>
+            <section className="flex flex-col p-[1rem] w-[56rem] bg-[#F6F6F6]">
                 <input
                     type="text"
                     value={formState.username}
@@ -51,11 +54,11 @@ const CommentCourses = () => {
                         setFormState({ ...formState, username: e.target.value })
                     }
                     placeholder=" Your Name"
-                    className="h-[3rem] w-[25rem] mb-[1rem] p-[1rem] text-[18px] bg-[#eae8e8]"
+                    className="h-[3rem] w-[54rem] mb-[1rem] p-[1rem] text-[18px] bg-[#fff] rounded-[25px]"
                 />
                 <MentionsInput
                     placeholder="Add Comment"
-                    className="h-[3rem] w-[25rem] mb-[1rem] p-[1rem] text-[18px] bg-[#eae8e8]"
+                    className="h-[7rem] w-[54rem] mb-[1rem] p-[1rem] text-[18px] bg-[#fff] rounded-[25px]"
                     value={formState.comment}
                     onChange={(e) =>
                         setFormState({ ...formState, comment: e.target.value })
@@ -63,21 +66,23 @@ const CommentCourses = () => {
                     >
                     <Mention data={users} />
                 </MentionsInput>
-                <button className="text-white bg-[#004458]  text-[1.2rem] p-[10px] mt-[1rem]" onClick={submit}>ثبت نظرات</button>
+                <button className="text-white bg-[#004458]  text-[1.2rem] p-[10px] mt-[1rem]  rounded-[20px]" onClick={submit}>ثبت نظرات</button>
             </section>
             {comments.length === 0 ? (
                 null
             ) : (
                 <section className="flex flex-row">
                     {comments.map((comment, i) => (
-                        <div className="  mt-[ 1.5rem] mb-[1.5rem] ml-[1rem] text-[1rem] p-[1rem] w-[25rem] bg-[#eae8e8] " key={i}>
+                        <div className="  mt-[ 1.5rem] mb-[1.5rem] ml-[1rem] text-[1rem] p-[1rem] w-[25rem] bg-[#fff]  rounded-[20px]" key={i}>
                             <p >
                                 {comment.username} on {date}
                             </p>
                             <h2>{comment.comment}</h2>
-                           <button className="bg-[#004458] w-[70px] h-[40px] text-[#fff]">پاسخ</button>
+                           
+                           <div className="bg-[#004458] w-[70px] h-[40px] text-[#fff] text-center rounded-[20px] leading-8">پاسخ</div>
                         </div>
                     ))}
+                
                 </section>
             )}
         </div>
