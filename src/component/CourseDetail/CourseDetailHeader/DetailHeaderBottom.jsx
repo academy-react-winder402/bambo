c5667310c0be99b45874fbf4b7a07cba4cdbf228
reza <rezakarimi74pnu@gmail.coma>
2024-05-09 21:21:44 +0330

    commentcourse

--- src/component/CourseDetail/CourseDetailHeader/DetailHeaderBottom.jsx
+++ src/component/CourseDetail/CourseDetailHeader/DetailHeaderBottom.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import teacher from "../../../assets/img/coursedetail/teacher.png"
 import like from "../../../assets/img/coursedetail/like.png"
 import star from "../../../assets/img/articledetail/star.jpg"
+import danesh from "../../../assets/img/coursedetail/danesh.png"
 const DetailHeaderBottom = () => {
     return(
- <div className="w-[100%] h-[250px] border-solid border-[#DBDBDB] border-[1px] flex flex-row-reverse text-center text-[40px] leading-[30s0px]">
-<div className="w-[33.5%] h-[250px] border-solid border-[#DBDBDB] border-[1px] flex flex-col justify-around">
+ <div className="w-[100%] h-[250px] border-solid border-[#DBDBDB] border-[1px] flex flex-row-reverse text-center text-[25px] leading-[30s0px]">
+<div className="w-[33.5%] h-[250px] border-solid border-[#DBDBDB] border-[1px] flex flex-col justify-center gap-[10px]">
     <img src={teacher} className="w-[30px] h-[30px] ml-[250px]"></img>
-    <span>مدرس : دکتر بحرالعلومی</span>
+    <div className="w-[140px] h-[130px]  ml-[190px] ">مدرس : دکتر بحرالعلومی</div>
 </div>
-<div className="w-[33.5%] h-[250px]  border-solid border-[#DBDBDB] border-[1px]"></div>
+<div className="w-[33.5%] h-[250px]  border-solid border-[#DBDBDB] border-[1px]  flex flex-col  gap-[10px]">
+<div className="w-[30px] h-[30px] ml-[250px] mt-[35px]" ><img src={danesh}></img></div>
+<div > تاریخ شروع : 1400/1/1</div>
+<div> تاریخ پایان : 1401/1/1</div>
+</div>
 <div className="w-[33.5%] h-[250px]  border-solid border-[#DBDBDB] border-[1px] ">
     <div className="w-[30px] h-[30px] m-auto mt-[3rem]"><img src={like}></img></div>
     <div >:رتبه</div>
--- src/component/CourseDetail/comment/CommentCourses.jsx
+++ src/component/CourseDetail/comment/CommentCourses.jsx
@@ -85,6 +85,7 @@ const CommentCourses = () => {
                 
                 </section>
             )}
+            
         </div>
     );
 };
