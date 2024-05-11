4459f2c8194a8b95a939b0dabe6795658cb9d4e4
reza <rezakarimi74pnu@gmail.coma>
2024-05-11 21:41:29 +0330

    editconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,7 @@ export default {
   theme: {
     extend: {
     },
-    screens:{
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-      'xs': '350px',
-      // => @media (min-width: 350px) { ... }
-    }
   },
   plugins: [],
 }
