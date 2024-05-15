3bf271189487a48f0c46f364e553daeda4b72a7c
reza <rezakarimi74pnu@gmail.coma>
2024-05-15 14:44:01 +0330

    env

--- src/component/common/searchlanding/contentsearch/Contentsearch.jsx
+++ src/component/common/searchlanding/contentsearch/Contentsearch.jsx
@@ -4,12 +4,11 @@ import js from '../../../../assets/img/courses/js.png';
 const Contentsearch = () => {
     return (
         <div className=" border-2 border-solid border-transparent w-[85%] h-[25rem]  m-auto bg-[#F1F1F1] ">
-            <div className="border-b border-solid border-[#004458]  h-[3.5rem] w-[45rem] m-auto mt-[1rem] flex justify-end 
-        hover:bg-[#004458] hover:cursor-pointer hover:text-[#fff]">
-
-
-
+            <div className="border-b border-solid border-[#088568] h-[2rem] m-auto w-[95%] flex justify-between mt-[2rem]">
+                <span className="block text-[#0CDBAB]"> مشاهده همه </span>
+                <h1 className="mr-[0.5rem]"> دوره ها </h1>
             </div>
+            <div className="border border-solid border-[#088568] mt-[1rem] h-[3rem] w-[95%] m-auto">  </div>
         </div>
     );
 };
