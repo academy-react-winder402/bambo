91d5a576b790075b2f44bae9096d07c56b8a1377
reza <rezakarimi74pnu@gmail.coma>
2024-05-15 19:52:33 +0330

    interceptor

---
+++ src/core/services/api/landing/countlanding.js
@@ -0,0 +1,14 @@
+import { instance } from "../../interseptor";
+
+const getcountlanding = async() => {
+    try{
+        console.log("fetching started ...");
+
+        const result = await instance.get("/Home/LandingReport");
+
+        return result;
+    }catch(error){
+        console.log(error);
+        return[];
+    }
+};
