07d00c5a1d9d01e93b85dcb9caf31fc743e5640c
reza <rezakarimi74pnu@gmail.coma>
2024-05-18 13:50:29 +0330

    registeredittttt

--- src/component/register/registerleft/RegisterLeft.jsx
+++ src/component/register/registerleft/RegisterLeft.jsx
@@ -68,11 +68,13 @@ const FormRegister = () => {
 
     };
     return (
-        <div dir="rtl" className="w-[55%]  my-auto">
+        <div dir="rtl" className="lg:w-[55%]  lg:my-auto
+        xs:w-[100%]  xs:my-auto 
+        ">
             <div className="lg:m-auto lg:w-[30rem] h-auto   lg:shadow-sm lg:border-solid lg:border-[1px] lg:border-[black] lg:rounded-lg lg:pb-[2rem] 
             sm:m-auto sm:w-[70%] sm:h-[100%]  sm:shadow-sm sm:border-[1px] sm:border-[black] sm:border-solid sm:rounded-lg 
             md:m-auto md:w-[70%] md:h-[100%] md:shadow-sm md:border-[1px] md:border-[black] md:border-solid md:rounded-lg md:mt-[20px]
-            xs:m-auto xs:w-[100%] xs:h-[100%] xs:bg-[#fff] 
+            xs:m-auto xs:w-[100%] xs:h-auto xs:bg-[#fff] 
             " >
             <RegisterLeftTop gotoleft={gotoleft}/>
             <div className="lg:pr-[40px] 
@@ -87,13 +89,13 @@ const FormRegister = () => {
         "><NavLink to="/login" >ورود</NavLink></div>
        <div className="lg:h-[50px] lg:w-[100%]  lg:mt-[40px]
         sm:h-[100px] sm:w-[500px] 
-      xs:h-[110px] xs:w-[300px] 
+      xs:h-[110px] xs:w-[300px] xs:m-auto
       md:h-[100px] md:w-[500px] 
        ">
       <button className="lg:h-[50px] lg:w-[80%] lg:block  lg:m-auto lg:hover:bg-gradient-to-b lg:from-[#004458] lg:to-[#1194bc]  lg:rounded-xl lg:bg-[#004458] lg:text-[#ffff]
                 sm:h-[50px] sm:w-[45%] sm:m-auto sm:hover:bg-gradient-to-b sm:from-[#004458] sm:to-[#1194bc]  sm:rounded-xl sm:bg-[#004458] sm:text-[#ffff]
                 md:h-[50px] md:w-[30%] md:m-auto md:hover:bg-gradient-to-b md:from-[#004458] md:to-[#1194bc]  md:rounded-xl md:bg-[#004458] md:text-[#ffff]
-                xs:h-[50px] xs:w-[45%] xs:m-auto xs:hover:bg-gradient-to-b xs:from-[#004458] xs:to-[#1194bc]  xs:rounded-xl xs:bg-[#004458] xs:text-[#ffff]
+                xs:h-[50px] xs:w-[100%] xs:mx-auto xs:hover:bg-gradient-to-b xs:from-[#004458] xs:to-[#1194bc]  xs:rounded-xl xs:bg-[#004458] xs:text-[#ffff]
                 "
                     disabled={page == 2}
                     onClick={() => {
