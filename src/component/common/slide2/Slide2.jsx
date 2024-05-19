97da7dbb7d8d5ab8dbd798ebbce580d44cc17aa0
reza <rezakarimi74pnu@gmail.coma>
2024-05-20 02:01:38 +0330

    slidertamam

---
+++ src/component/common/slide2/Slide2.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles2.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Rightimg } from '../../landing/box5/item/right/Rightimg';
+
+const Slide2 = ({news}) => {
+    return (
+
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="w-[40rem] h-[25rem] rounded-md"
+            >
+        {news.map((item, index) => {
+          return (<SwiperSlide key={index} im={item.tumbImageAddress}>
+            <Rightimg key={index} im={item.tumbImageAddress}  />
+          </SwiperSlide>)
+        })}
+            </Swiper>
+        </>
+    );
+};
+
+export { Slide2 };
--- src/component/landing/box5/Box5.jsx
+++ src/component/landing/box5/Box5.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import { Rightt } from "./item/Rightt";
+import React,{useState, useEffect} from "react";
+import {Rightt} from './item/right/Rightt';
 import { Leftt } from './item/Leftt';
+
+import { getnewslanding } from "../../../core/services/api/landing/newslanding";
 
 
 
 const Box5 = () => {
+
+
+    const [newslanding, setnewslanding] = useState([]);
+
+    const getNewsList = async () => {
+        const courses = await getnewslanding();
+        setnewslanding(courses);
+    };
+
+    useEffect(() => {
+        getNewsList();
+      
+    }, []);
+
+
     return (
         <div className=" h-[46rem] w-[96rem]  flex justify-between m-auto dark:bg-[#242424]">
-            <Rightt />
+            <Rightt news={newslanding}/>
             <Leftt  />
         </div>
     );
