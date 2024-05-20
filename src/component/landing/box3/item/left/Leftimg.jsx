3b6e142f601d1a821ac30d5f8b142d4acb476374
reza <rezakarimi74pnu@gmail.coma>
2024-05-20 10:25:11 +0330

    fdf

--- src/component/landing/box3/item/left/Leftimg.jsx
+++ src/component/landing/box3/item/left/Leftimg.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Leftimg = ({ im, title, teacher, typename }) => {
 
     return (
-        <div className=" h-[30rem] w-[100%] m-auto  relative group duration-[2000ms]">
+        <div className=" h-[22rem] w-[15rem] m-auto  relative group duration-[2000ms]">
 
-            <img src={im} className="h-[30rem] w-[20rem] rounded-md " />
+            <img src={im} className="h-[100%] w-[100%] rounded-md " />
             <div className="h-[100%] w-[100%] bg-[#000000a0] absolute top-0 left-[0] text-white group-hover:left-0 opacity-0 
         group-hover:opacity-[1] rounded-md duration-[1500ms]">
-                <div className="text-center text-3xl mt-[5rem]"> {title}  </div>
-                <div className=" text-xl mt-[5rem] text-right mr-[2rem]"> {teacher} <span> : </span> <span> نام استاد </span> </div>
-                <div className=" text-xl mt-[3rem] text-right mr-[2rem]">  <span> نوغ دوره </span> <span> : </span> {typename}   </div>
+                <div className="text-center text-xl mt-[3rem]"> {title}  </div>
+                <div className=" text-lg mt-[3rem] text-right mr-[0.5rem]"> {teacher} <span> : </span> <span> نام استاد </span> </div>
+                <div className=" text-lg mt-[1rem] text-right mr-[0.5rem]">  <span> نوغ دوره </span> <span> : </span> {typename}   </div>
 
             </div>
         </div>
--- src/core/services/api/landing/courselanding.js
+++ src/core/services/api/landing/courselanding.js
@@ -5,7 +5,7 @@ const getcourselanding = async() => {
     try{
         console.log("fetching started ...");
 
-        const result = await instance.get("/Home/GetCoursesTop");
+        const result = await instance.get("/Home/GetCoursesTop?count=3");
 
         return result;
     }catch(error){
