e5c09b9be4bbaa1f57542abbd23c47849e195645
reza <rezakarimi74pnu@gmail.coma>
2024-05-20 08:31:36 +0330

    fdf

--- src/component/common/searchlanding/contentsearch/Contentsearch.jsx
+++ src/component/common/searchlanding/contentsearch/Contentsearch.jsx
@@ -3,12 +3,15 @@ import js from '../../../../assets/img/courses/js.png';
 
 const Contentsearch = () => {
     return (
-        <div className=" border-2 border-solid border-transparent w-[85%] h-[25rem]  m-auto bg-[#F1F1F1] ">
-            <div className="border-b border-solid border-[#088568] h-[2rem] m-auto w-[95%] flex justify-between mt-[2rem]">
-                <span className="block text-[#0CDBAB]"> مشاهده همه </span>
-                <h1 className="mr-[0.5rem]"> دوره ها </h1>
-            </div>
-            <div className="border border-solid border-[#088568] mt-[1rem] h-[3rem] w-[95%] m-auto">  </div>
+        <>
+
+
+            <div className=" border-2 border-solid border-transparent w-[85%] h-[25rem]  m-auto bg-[#F1F1F1] ">
+                <div className="border-b border-solid border-[#088568] h-[2rem] m-auto w-[95%] flex justify-between mt-[2rem]">
+                    <span className="block text-[#0CDBAB]"> مشاهده همه </span>
+                    <h1 className="mr-[0.5rem]"> دوره ها </h1>
+                </div>
+                <div className="border border-solid border-[#088568] mt-[1rem] h-[3rem] w-[95%] m-auto">  </div>
 
             </div>
             <div className="border border-solid border-[#088568] mt-[1rem] h-[5rem] w-[95%] m-auto">
@@ -23,7 +26,10 @@ const Contentsearch = () => {
 
             </div>
 
-        </div>
+        
+        
+        </>
+
     );
 };
 
