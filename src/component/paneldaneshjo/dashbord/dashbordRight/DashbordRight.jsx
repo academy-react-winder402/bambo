4dcc4599beeb224de298bdba65425ed19494e1cc
reza <rezakarimi74pnu@gmail.coma>
2024-05-25 18:13:52 +0330

    dashbordsave

--- src/component/paneldaneshjo/dashbord/dashbordRight/DashbordRight.jsx
+++ src/component/paneldaneshjo/dashbord/dashbordRight/DashbordRight.jsx
@@ -5,6 +5,16 @@ const Right =()=>{
   <div className=" lg:w-[90%] lg:h-[13%] lg:m-auto lg:border-b-solid lg:border-b-[1px] lg:border-b-[#088568] lg:text-[#088568] lg:text-[20px]
    lg:pt-[30px] 
     ">: اطلاعات ثبت شده شما</div>
+    <div className="lg:w-[90%] lg:h-[80%] lg:bg-[#F1F1F1] lg:m-auto lg:mt-[20px]">
+        <div className="lg:w-[100%] lg:h-[16%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>
+        <div className="lg:w-[100%] lg:h-[14%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>  
+          <div className="lg:w-[100%] lg:h-[14%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>
+          <div className="lg:w-[100%] lg:h-[14%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>
+          <div className="lg:w-[100%] lg:h-[14%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>
+          <div className="lg:w-[100%] lg:h-[14%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>
+          <div className="lg:w-[100%] lg:h-[14%]  lg:border-[red] lg:border-[1px] lg:border-solid"></div>
+
+    </div>
 
         </div>
     )
