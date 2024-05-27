9aafde2effe3493b3fbd490c45f78d5cc3d9c155
reza <rezakarimi74pnu@gmail.coma>
2024-05-27 23:13:03 +0330

    editpanell

--- src/component/paneldaneshjo/pannelcourse/PannelCourse.jsx
+++ src/component/paneldaneshjo/pannelcourse/PannelCourse.jsx
@@ -13,7 +13,7 @@ const PannelCourse = () => {
             <div className=" lg:w-[90%] lg:h-[13%] lg:m-auto lg:border-b-solid lg:border-b-[1px] lg:border-b-[#088568] lg:text-[#088568] lg:text-[20px]
    lg:pt-[30px] 
     ">لیست دوره ها</div>
-            <div className=" lg:w-[90%] lg:m-auto lg:border-solid lg:border-[1px] lg:border-[#088568] lg:text-[#088568]">
+            <div className=" lg:w-[90%] lg:m-auto lg:text-[#088568]">
                 <div className="lg:w-[100%] lg:h-[60px] lg:bg-[#09B28B] lg:mt-[20px] lg:flex lg:text-[#fff] lg:text-center lg:leading-[50px]">
                     <div className="lg:w-[16%] lg:h-[100%] ">عکس دوره</div>
                     <div className="lg:w-[16%] lg:h-[100%] ">نام دوره</div>
