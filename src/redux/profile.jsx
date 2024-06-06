a52c4134db39e94116b8c45ad30681ce46f02048
reza <rezakarimi74pnu@gmail.coma>
2024-06-06 12:51:58 +0330

    editimmg

--- src/redux/profile.jsx
+++ src/redux/profile.jsx
@@ -3,22 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const profile = createSlice ({
     name : "profile",
     initialState:{
-LName : "reza",
-FName : "karimi",
-HomeAdderess:"sari",
-PhoneNumber:"09386012036",
-BirthDay:"24 mehr",
-Gmail:"www.leilakarimi2015@gmail.com",
-Gender:"animal"
+
     },
     reducers:{
-setlname:(state,action) =>{
-    state.LName=action.payload;
-},
-setfname:(state,action) =>{
-    state.FName=action.payload;
-},
+
     }
 });
-export const{setfname,setlname}=profile.actions;
+
 export default profile.reducer
