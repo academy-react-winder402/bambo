3ce9075c33e39719fe737db3dcdeecf25051d33a
reza <rezakarimi74pnu@gmail.coma>
2024-06-06 12:51:37 +0330

    editimg

--- src/component/paneldaneshjo/editprofile/EditProfile.jsx
+++ src/component/paneldaneshjo/editprofile/EditProfile.jsx
@@ -1,35 +1,37 @@
 import { Formik, Form, Field } from "formik";
 import React, { useEffect, useState } from "react";
 import { Fragment } from "react";
-import { useDispatch } from "react-redux";
-import { setlname } from "../../../redux/profile";
 import { getname } from "../../../core/services/api/paneldaneshjo/EditProfile";
 import { useParams } from "react-router-dom";
+import { AddImg } from "../../../core/services/api/paneldaneshjo/EditProfile";
 
 const EditProfile = () => {
-  const [profile,setProfile] = useState([])
-  const id= useParams().id;
-  
+  const [profile, setProfile] = useState([]);
+  const [user, setUser] = useState();
+  const id = useParams().id;
+
   console.log(id);
-  const onSubmit = async(values)=>{
-    const formdata =new FormData();
-formdata.append ("LName",values.LName)
-formdata.append ("FName",values.FName)
-formdata.append ("BirthDay",values.BirthDay)
-formdata.append ("phoneNumber",values.phoneNumber)
-formdata.append ("Gender",values.Gender)
-formdata.append ("HomeAdderess",values.HomeAdderess)
-formdata.append ("Email",values.Email)
+  const onSubmit = async (values) => {
+    const formdata = new FormData();
+    formdata.append("LName", values.LName);
+    formdata.append("FName", values.FName);
+    formdata.append("BirthDay", values.BirthDay);
+    formdata.append("phoneNumber", values.phoneNumber);
+    formdata.append("Gender", values.Gender);
+    formdata.append("HomeAdderess", values.HomeAdderess);
+    formdata.append("Email", values.Email);
 
-const editapi = await getname(formdata);
+    const editapi = await getname(formdata);
 
-console.log(editapi);
+    console.log(editapi);
+  };
+  const [pic,setPic] = useState([]);
+const getimg =async()=>{
+  const imgdata = new FormData();
 
-  }
-
-
-
-
+  imgdata.append('image', values.img);
+  const editimg = await AddImg(imgdata);
+}
   return (
     <Fragment>
       <div
@@ -47,23 +49,23 @@ xs:py-[10px]
       </div>
 
       <div
-        className="lg:w-[90%] lg:m-auto lg:h-[85%]  md:w-[90%]  md:m-auto sm:w-[90%] sm:m-auto xs:w-[90%]  xs:m-auto 
-    "
-      >
-        <Formik
-        initialValues={{LName:profile.LName,FName:profile.FName,Email:profile.Email,BirthDay:profile.BirthDay,phoneNumber:profile.phoneNumber,Gender:profile.Gender,HomeAdderess:profile.HomeAdderess}}
-        onSubmit={(values) => onSubmit(values)}
-        >
-          <Form>
-            <div className="flex flex-row-reverse lg:w-[100%] mt-[20px] h-[100%] ">
-              <div className="lg:w-[30%] flex flex-col gap-[10px] items-center ">
-                <img className="object-cover w-40 h-40 p-1 rounded-full ring-2 ring-indigo-300 dark:ring-indigo-500"></img>
+        className="lg:w-[90%] lg:m-auto lg:h-[85%]  md:w-[90%]  md:m-auto sm:w-[90%] sm:m-auto xs:w-[90%]  xs:m-auto " >
+        <div className="flex flex-row-reverse lg:w-[100%] mt-[20px] h-[100%] ">
+        <Formik initialValues={{img:pic.img}}>
+        {(form) => <div className="lg:w-[30%] flex flex-col gap-[10px] items-center ">
+           
+              <Form>
+              <div className="w-[300px] h-[300px] border-[1px] border-black border-solid rounded-[100%]">
+                            {form.values.img && <img src={URL.createObjectURL(form.values.img)} className="object-cover w-40 h-40 p-1 rounded-full ring-2 ring-indigo-300 dark:ring-indigo-500"/>}
+
+                            <Field type="file" name="img" onChange={(e) => form.setFieldValue('img', e.target.files[0])} />
+                        </div>
 
                 <div className="flex flex-col gap-[20px]">
                   <button
                     type="button"
                     className="py-3.5 px-7 text-base font-medium text-[#fff] focus:outline-none bg-[#09B28B]  rounded-xl "
-                  >
+                 onClick={getimg} >
                     Change picture
                   </button>
                   <button
@@ -73,23 +75,36 @@ xs:py-[10px]
                     Delete picture
                   </button>
                 </div>
-              </div>
-
-              <div className="lg:w-[70%] flex flex-col gap-[30px] items-start text-[#202142] h-[100%] ">
+              </Form>
+           
+          </div>}
+          </Formik>
+          <div className="lg:w-[70%] flex flex-col gap-[30px] items-start text-[#202142] h-[100%] ">
+            <Formik
+              initialValues={{
+                LName: profile.LName,
+                FName: profile.FName,
+                Email: profile.Email,
+                BirthDay: profile.BirthDay,
+                phoneNumber: profile.phoneNumber,
+                Gender: profile.Gender,
+                HomeAdderess: profile.HomeAdderess,
+              }}
+              onSubmit={(values) => onSubmit(values)}
+            >
+              <Form>
                 <div className="lg:flex lg:gap-[20px] items-center w-[100%] space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 ">
                   <div className="w-[33%]">
                     <label
                       for="LName"
                       className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white"
                     >
-                   نام
+                      نام
                     </label>
                     <Field
                       type="text"
                       name="LName"
-                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                   
-                    />
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "  />
                   </div>
 
                   <div className="w-[33%]">
@@ -102,9 +117,7 @@ xs:py-[10px]
                     <Field
                       type="text"
                       name="FName"
-                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                    
-                    />
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 " />
                   </div>
                 </div>
 
@@ -119,7 +132,7 @@ xs:py-[10px]
                     <Field
                       type="Email"
                       className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                     name="Email"
+                      name="Email"
                     />
                   </div>
 
@@ -128,12 +141,12 @@ xs:py-[10px]
                       for="BirthDay"
                       className=" mb-2 text-sm font-medium text-indigo-900 dark:text-white"
                     >
-                     تاریخ تولد
+                      تاریخ تولد
                     </label>
                     <Field
                       type="text"
                       className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                     name="BirthDay"
+                      name="BirthDay"
                     />
                   </div>
                 </div>
@@ -148,9 +161,7 @@ xs:py-[10px]
                     <Field
                       type="phoneNumber"
                       name="PhoneNumber"
-                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                    
-                    />
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 " />
                   </div>
 
                   <div className="w-[33%]">
@@ -158,35 +169,27 @@ xs:py-[10px]
                       for="Gender"
                       className=" mb-2 text-sm font-medium text-indigo-900 dark:text-white"
                     >
-                     جنسیت
+                      جنسیت
                     </label>
                     <Field
-                    name="Gender"
+                      name="Gender"
                       type="text"
-                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                     
-                    />
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 " />
                   </div>
-
                 </div>
                 <div className="w-[100%]  ">
-                    <label
-                      for="HomeAdderess"
-                      className=" mb-2 text-sm font-medium text-indigo-900 dark:text-white"
-                    >
-                      آدرس
-                    </label>
-                    <Field
+                  <label
+                    for="HomeAdderess"
+                    className=" mb-2 text-sm font-medium text-indigo-900 dark:text-white"
+                  >
+                    آدرس
+                  </label>
+                  <Field
                     name="HomeAdderess"
-                      type="text"
-                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                      
-                    />
-                  
-
-              
+                    type="text"
+                    className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                  />
                 </div>
-
                 <div
                   className="lg:w-[100%]  lg:flex lg:justify-end lg:gap-[30px] lg:text-center lg:text-[17px] lg:mt-[30px] lg:text-[#fff]
     md:w-[100%]  md:flex md:justify-end md:gap-[30px] md:text-center md:text-[17px] md:mt-[30px] md:text-[#fff]
@@ -209,68 +212,15 @@ md:bg-[#09B28B] md:w-[17%] md:h-[50px]
 sm:bg-[#09B28B] sm:w-[17%] sm:h-[50px]
 xs:bg-[#09B28B] xs:w-[30%] xs:h-[50px]
 "
-type="onsubmit"
-
+                    type="onsubmit"
                   >
                     ثبت تغییرات
                   </button>
                 </div>
-              </div>
-            </div>
-            {/* <Field placeholder="نام" className="lg:w-[45%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%] lg:mr-[7%] pr-[20px] text-[15px] text-[#004458]
-md:w-[45%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[0%]
-sm:w-[45%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[0%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
- "/>
-<Field placeholder="نام خانوادگی" className="lg:w-[45%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%] lg:mr-[3%]
-md:w-[45%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[10%]
-sm:w-[45%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[10%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
-"/>
-<Field placeholder="نام کاربری" className="lg:w-[45%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%] lg:mr-[7%]
-md:w-[45%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[0%]
-sm:w-[45%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[0%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
-"/>
-<Field placeholder="تاریخ تولد" className="lg:w-[45%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%]  lg:mr-[3%]
-md:w-[45%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[10%]
-sm:w-[45%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[10%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
-"/>
-<Field placeholder="شماره موبایل" className="lg:w-[45%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%] lg:mr-[7%]
-md:w-[45%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[0%]
-sm:w-[45%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[0%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
-"/>
-<Field placeholder="شماره ملی" className="lg:w-[45%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%]  lg:mr-[3%]
-md:w-[45%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[10%]
-sm:w-[45%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[10%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
-"/>
-<Field placeholder="ایمیل" className="lg:w-[93%] lg:h-[50px] lg:bg-[#DBDBDB] lg:mt-[5%] lg:mr-[7%]
-md:w-[100%] md:h-[50px] md:bg-[#DBDBDB] md:mt-[5%] md:mr-[0%]
-sm:w-[100%] sm:h-[50px] sm:bg-[#DBDBDB] sm:mt-[5%] sm:mr-[0%]
-xs:w-[90%] xs:h-[40px] xs:bg-[#DBDBDB] xs:mt-[5%] xs:mr-[5%]
-"/>   
-   
-    <div className="lg:w-[100%] lg:h-[10%] lg:flex lg:justify-end lg:gap-[30px] lg:text-center lg:text-[17px] lg:mt-[30px] lg:text-[#fff]
-    md:w-[100%] md:h-[10%] md:flex md:justify-end md:gap-[30px] md:text-center md:text-[17px] md:mt-[30px] md:text-[#fff]
-    sm:w-[100%] sm:h-[10%] sm:flex sm:justify-end sm:gap-[30px] sm:text-center sm:text-[17px] sm:mt-[30px] sm:text-[#fff]
-    xs:w-[100%] xs:h-[50px] xs:flex xs:justify-end xs:gap-[30px] xs:text-center xs:text-[17px] xs:mt-[30px] xs:text-[#fff]
-    ">
-<button className="lg:bg-[#F83333] lg:w-[17%] lg:h-[100%]
-md:bg-[#F83333] md:w-[17%] md:h-[100%]
-sm:bg-[#F83333] sm:w-[17%] sm:h-[100%]
-xs:bg-[#F83333] xs:w-[30%] xs:h-[70%]
-">لغوتغییرات</button>
-<button className="lg:bg-[#09B28B] lg:w-[17%] lg:h-[100%]
-md:bg-[#09B28B] md:w-[17%] md:h-[100%]
-sm:bg-[#09B28B] sm:w-[17%] sm:h-[100%]
-xs:bg-[#09B28B] xs:w-[30%] xs:h-[70%]
-">ثبت تغییرات</button>
-    </div> */}
-          </Form>
-        </Formik>
+              </Form>
+            </Formik>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
